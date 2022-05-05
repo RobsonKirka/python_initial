2952a4362e4b601fa12ca846ba6ea6e2328a0a18
robsonkirka <robsonfernandes_job@hotmail.com>
2022-05-05 19:34:40 -0300

    add file read

--- .vscode/forca.py
+++ .vscode/forca.py
@@ -1,4 +1,5 @@
 from ast import comprehension
+import random
 
 
 def jogar():
@@ -6,7 +7,16 @@ def jogar():
     print("***Jogo de forca***")
     print("*******************")
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
     letras_acertadas = ['_' for letra in palavra_secreta]  # list comprehension
 
     enforcou = False
